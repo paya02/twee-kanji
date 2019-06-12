5fac7603e15b1161342241fd42b310f5e9d9a788
paya02 <45tissue.pa8@gmail.com>
2019-06-12 14:30:54 +0900

    gemにdevise追加。Userモデル作成 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  devise_for :users
+  #root 'application#hello'
+  root 'homes#index'
+
+  get 'homes/index'
 end
