7a0bd64db1c13c570389b0b302d5d57def95d460
paya02 <45tissue.pa8@gmail.com>
2019-06-15 12:15:03 +0900

    DBをpostgresに変更

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2019_06_12_044717) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
