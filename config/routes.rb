aa888bf24bf4aa2b592d551d48611758c072ba45
paya02 <45tissue.pa8@gmail.com>
2019-06-22 12:41:43 +0900

    TOPページをログイン画面に変更 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-  root 'homes#index'
+  
+  devise_scope :user do
+    root to: "devise/sessions#new"
+    get 'login', to: 'devise/sessions#new'
+  end
 
   get 'homes/index'
 end
