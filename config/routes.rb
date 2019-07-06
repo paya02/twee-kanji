de4881aa1dc23ac205c60afb1fbb087a8c8f54fb
paya02 <45tissue.pa8@gmail.com>
2019-07-06 18:45:52 +0900

    イベントコントローラーのルーティング追加 #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'events/show'
-  get 'events/add'
-  get 'events/edit'
+  get 'events/:id', to: 'events#show'
+
+  get 'events/add'  
+  post 'events/add', to: 'events#create'
+
+  get 'events/edit/:id', to: 'events#edit'
+  patch 'events/edit/:id', to: 'events#update'
+
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   
   devise_scope :user do
