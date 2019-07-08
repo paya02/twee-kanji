f47f19e48f6cce5906bebeaf12b2d9db0fc8420c
paya02 <45tissue.pa8@gmail.com>
2019-07-08 14:32:27 +0900

    テスト用のデータを定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: 'test_twitter@example.com',
+  encrypted_password: 'test123',
+  reset_password_token: 'test',
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  provider: 'twitter',
+  uid: 'test_twitter1234567890',
+  username: 'TestTwitterUser',
+  nickname: 'test_twitter_user'
+)
