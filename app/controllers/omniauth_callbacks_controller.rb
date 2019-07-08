c1d5612a3d54ef3646ac4bdba6a2592e4398fed7
paya02 <45tissue.pa8@gmail.com>
2019-07-08 14:36:52 +0900

    ログイン後のリダイレクト先をイベント作成ページに変更 #2

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,15 +7,20 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_from(provider)
     provider = provider.to_s
-
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-
+    
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
+      # 前
       #session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to root_url
     end
+  end
+
+  def after_sign_in_path_for(resource)
+    # sign_in_and_redirectの中でコールされる
+    events_add_url
   end
 end
