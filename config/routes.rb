ab7bec0cd2d35cd2a23c15f9aa9e7f95c1eda0d9
paya02 <45tissue.pa8@gmail.com>
2019-07-08 14:09:46 +0900

    ルートをhomeコントローラのindexアクションに変更 #2

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   
   devise_scope :user do
-    root to: "devise/sessions#new"
     get 'login', to: 'devise/sessions#new'
   end
 
-  root to:
+  root to: 'homes#index'
   get 'homes/index'
 end
