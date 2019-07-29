474fda8458d8d9fc80be839c6dec4acf2d6c4faa
paya02 <45tissue.pa8@gmail.com>
2019-07-29 13:57:28 +0900

    jQuery-uiライブラリファイルを追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require jquery-ui/i18n/datepicker-ja
 //= require_tree .
