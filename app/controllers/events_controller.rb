5269137929bfe7647123774f58198dd1bfa1ebc3
paya02 <45tissue.pa8@gmail.com>
2019-07-29 14:14:49 +0900

    イベント作成追加、紹介ページを追加。指定可能な日付候補の数を定数から呼び出す #2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,39 @@
 class EventsController < ApplicationController
+  protect_from_forgery
+
   def show
+    @event = Event.find(params[:id])
   end
 
   def add
+    @date_cnt = APPSETTINGS::MAX_DATE_CNT
     @event = Event.new
   end
 
+  def create
+    if request.post? then
+      @event = Event.new event_params
+      
+      # 候補日付をカンマ区切りで保存
+      csv_date = ""
+      params[:date_val].each do |date|
+        csv_date += date + ','
+      end
+      @event.date_list = csv_date
+
+      if @event.save then
+        render action: 'show', id: @event.id
+      else
+        render 'add'
+      end
+    end
+  end
+
   def edit
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :url, :fee, :detail, date_val: [])
   end
 end
