6c14774f99f9d5eae3100b840f178b6d0acb6583
paya02 <45tissue.pa8@gmail.com>
2019-08-02 13:10:00 +0900

    callback_urlを環境変数に設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,7 +261,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   #config.omniauth :twitter, "API key", "API secret key", callback_url: "Thttp://localhost:3002/users/omniauth_callbacks"
-  config.omniauth :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret], display: 'popup', callback_url: "http://localhost:3002/users/omniauth_callbacks"
+  config.omniauth :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret], display: 'popup', callback_url: ENV['CALLBACK_URL']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
