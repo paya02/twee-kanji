870ff29fe28f8b48d595bc07567869e45a417ae3
paya02 <45tissue.pa8@gmail.com>
2019-10-13 16:26:07 +0900

    Add サンプル用の初期投入データを追加 #11

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User ---------------------------------
 User.create!(
   email: 'test_twitter@example.com',
   encrypted_password: 'test123',
@@ -12,7 +13,43 @@ User.create!(
   reset_password_sent_at: nil,
   remember_created_at: nil,
   provider: 'twitter',
-  uid: 'test_twitter1234567890',
-  username: 'TestTwitterUser',
-  nickname: 'test_twitter_user'
+  uid: 'sample_kanji_user',
+  username: 'サンプル幹事',
+  nickname: 'twi-kanji_sample'
 )
+
+(1..3).each do |cnt|
+  User.create!(
+    email: 'test_twitter@example.com',
+    encrypted_password: 'test123',
+    reset_password_token: 'test',
+    reset_password_sent_at: nil,
+    remember_created_at: nil,
+    provider: 'twitter',
+    uid: 'sample_memver_user' + cnt.to_s,
+    username: 'サンプル参加者' + cnt.to_s,
+    nickname: 'sample_user' + cnt.to_s
+  )
+end
+
+# event ---------------------------------
+Event.create!(
+  user_id: 21,
+  title: 'サンプルイベント', 
+  detail: 'サンプルイベントの詳細です。参加者の方により詳細なイベントの情報を伝えることができます。' 
+);
+
+# member ---------------------------------
+Member.create!(
+  event_id: 139,
+  user_id: 21
+);
+
+# decisions ---------------------------------
+(5..7).each do |cnt|
+  Decision.create!(
+    event_id: 139,
+    user_id: 21,
+    day: Date.parse("2019/10/2" + cnt.to_s)
+  );
+end
