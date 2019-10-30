a5e869f073fde87d94a19ca1866816623c205872
paya02 <45tissue.pa8@gmail.com>
2019-10-30 11:58:49 +0900

    Change テストのitを変更 #13

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "メールアドレス、パスワードのバリデーション" do
+  it "ユーザモデルのバリデーション" do
     user = User.create(
       email: 'test@example.com',
       password: 123456,
