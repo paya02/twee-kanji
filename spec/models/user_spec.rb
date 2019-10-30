8c0c4be5dc1deb35b6b1400ff0ca014eb98e41fc
paya02 <45tissue.pa8@gmail.com>
2019-10-30 11:24:25 +0900

    Add Userモデルのテストファイル追加 #13

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "メールアドレス、パスワードのバリデーション" do
+    user = User.create(
+      email: 'test@example.com',
+      password: 123456,
+      encrypted_password: 111
+    )
+    expect(user).to be_valid
+  end
+
+end
