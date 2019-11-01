8fc62bbc5a157ab309210b22f1dde8a94f4c78c7
paya02 <45tissue.pa8@gmail.com>
2019-11-01 18:46:12 +0900

    Add RSpec用スペックファイルのジェネレーターを変更 #13

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,14 @@ module Myapp
     # the framework and any gems in your application.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+    
   end
 end
