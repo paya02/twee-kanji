4494c779f8e7642f96a89476f77f93b213198a19
paya02 <45tissue.pa8@gmail.com>
2019-11-04 21:20:27 +0900

    Add Eventコントローラーのテストを追加 #13

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe EventsController, type: :controller do
   describe "#index" do
     context "認証済みのユーザーとして" do
       before do
-        @user_i = User.create(email: 'test@example.com', password: 123456, encrypted_password: 111)
+        @user = FactoryBot.create(:user)
       end
 
       it "正常にレスポンスを返すこと" do
-        sign_in @user_i
+        sign_in @user
         get :index
         expect(response).to be_success
       end
 
       it "200レスポンスを返すこと" do
-        sign_in @user_i
+        sign_in @user
         get :index
         expect(response).to have_http_status "200"
       end
@@ -34,14 +34,158 @@ RSpec.describe EventsController, type: :controller do
     end
   end
 
-  # describe "#create" do
-  #   context "認証済みのユーザーとして" do
-  #     before do
-  #       @user_c = User.create(email: 'test@example.com', password: 123456, encrypted_password: 111)
-  #     end
+  describe "#create" do
+    context "認証済みのユーザーとして" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      
+      it "プロジェクトを追加できること" do
+        sign_in @user
+        event_params = FactoryBot.attributes_for(:event, user_id: @user.id)
+        date_val_params = ['2019/01/01','2019/01/02']
+        expect {
+          post :create, params: { event: event_params, date_val: date_val_params}
+        }.to change(Event, :count).by(1)
+      end
+    end
 
-  #     it "プロジェクトを追加できること" do
-  #     end
-  #   end
-  # end
+    context "ゲストとして" do
+      it "302レスポンスを返すこと" do
+        event_params = FactoryBot.attributes_for(:event)
+        date_val_params = ['2019/01/01','2019/01/02']
+        post :create, params: { event: event_params, date_val: date_val_params }
+        expect(response).to have_http_status "302"
+      end
+
+      it "トップページにリダイレクトすること" do
+        event_params = FactoryBot.attributes_for(:event)
+        date_val_params = ['2019/01/01','2019/01/02']
+        post :create, params: { event: event_params, date_val: date_val_params }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "#update" do
+    context "認可されたユーザーとして" do
+      before do
+        @user = FactoryBot.create(:user)
+        @event = FactoryBot.create(:event, user_id: @user.id)
+        @member = FactoryBot.create(:member, event_id: @event.id, user_id: @user.id)
+      end
+
+      it "プロジェクトを更新できること" do
+        sign_in @user
+        event_params = FactoryBot.attributes_for(:event, title: "New Project Name")
+        date_val_params = ['2019/01/01','2019/01/02']
+        patch :update, params: { id: @event.id, event: event_params, date_val: date_val_params }
+        expect(@event.reload.title).to eq "New Project Name"
+      end
+    end
+
+    context "認可されていないユーザーとして" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @event = FactoryBot.create(:event, user_id: other_user.id, title: "Same Old Name")
+        @member = FactoryBot.create(:member, event_id: @event.id, user_id: other_user.id)
+      end
+
+      it "プロジェクトを更新できないこと" do
+        sign_in @user
+        event_params = FactoryBot.attributes_for(:event, title: "New Name")
+        patch :update, params: { id: @event.id, event: event_params }
+        expect(@event.reload.title).to eq "Same Old Name"
+      end
+
+      it "トップページにリダイレクトすること" do
+        sign_in @user
+        event_params = FactoryBot.attributes_for(:event)
+        patch :update, params: { id: @event.id, event: event_params }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "ゲストとして" do
+      before do
+        @user = FactoryBot.create(:user)
+        @event = FactoryBot.create(:event, user_id: @user.id)
+      end
+
+      it "302レスポンスを返すこと" do
+        event_params = FactoryBot.attributes_for(:event)
+        date_val_params = ['2019/01/01','2019/01/02']
+        patch :update, params: { id: @event.id, event: event_params, date_val: date_val_params }
+        expect(response).to have_http_status "302"
+      end
+
+      it "トップページにリダイレクトすること" do
+        event_params = FactoryBot.attributes_for(:event)
+        date_val_params = ['2019/01/01','2019/01/02']
+        patch :update, params: { id: @event.id, project: event_params }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "#destroy" do
+    context "認可されたユーザーとして" do
+      before do
+        @user = FactoryBot.create(:user)
+        @event = FactoryBot.create(:event, user_id: @user.id)
+      end
+
+      it "プロジェクトを削除できること" do
+        sign_in @user
+        expect {
+          delete :destroy, params: { id: @event.id }
+        }.to change(Event, :count).by(-1)
+      end
+    end
+
+    context "認可されていないユーザーとして" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @event = FactoryBot.create(:event, user_id: other_user.id)
+      end
+
+      it "プロジェクトを削除できないこと" do
+        sign_in @user
+        expect {
+          delete :destroy, params: { id: @event.id }
+        }.to_not change(Event, :count)
+      end
+
+      it "トップページにリダイレクトすること" do
+        sign_in @user
+        delete :destroy, params: { id: @event.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "ゲストとして" do
+      before do
+        @user = FactoryBot.create(:user)
+        @event = FactoryBot.create(:event, user_id: @user.id)
+      end
+
+      it "302レスポンスを返すこと" do
+        delete :destroy, params: { id: @event.id }
+        expect(response).to have_http_status "302"
+      end
+
+      it "サインイン画面にリダイレクトすること" do
+        delete :destroy, params: { id: @event.id }
+        expect(response).to redirect_to root_path
+      end
+
+      it "プロジェクトを削除できないこと" do
+        expect {
+          delete :destroy, params: { id: @event.id }
+        }.to_not change(Event, :count)
+      end
+     end
+  end
 end
